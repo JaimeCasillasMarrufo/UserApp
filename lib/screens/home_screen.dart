2b5346c109b23edf9a3d06330ec2556bc14c9785
JaimeCasillasMarrufo <jaimecasillasmarrufo@gmail.com>
2024-11-15 21:29:35 -0600

    Añadí la pantalla de inicio con navegación al perfil

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'create_profile_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => CreateProfileScreen()),
+            );
+          },
+          child: Text('Crear perfil'),
+        ),
+      ),
+    );
+  }
+}
