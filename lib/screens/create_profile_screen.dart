5c774b054a1c990750ab51337a988f50ed6758c6
JaimeCasillasMarrufo <jaimecasillasmarrufo@gmail.com>
2024-11-15 21:30:13 -0600

    Añadí la pantalla de crear pefil con los textos y navegación

---
+++ lib/screens/create_profile_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'profile_screen.dart';
+
+class CreateProfileScreen extends StatelessWidget {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController ageController = TextEditingController();
+  final TextEditingController occupationController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Crear Perfil'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'Nombre'),
+            ),
+            TextField(
+              controller: ageController,
+              decoration: InputDecoration(labelText: 'Edad'),
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              controller: occupationController,
+              decoration: InputDecoration(labelText: 'Ocupación'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProfileScreen(
+                      name: nameController.text,
+                      age: ageController.text,
+                      occupation: occupationController.text,
+                    ),
+                  ),
+                );
+              },
+              child: Text('Guardar y Ver Perfil'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
